d6aa0a5f2d9514563652868a7d1cc9ccc522b100
Level-Up-4 <vikash-verma-profile>
2022-06-16 17:51:24 +0530

    add

--- Microservices/demo/src/main/java/com/example/demo/HelloWorldController.java
+++ Microservices/demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 //controller
 public class HelloWorldController {
 
@@ -12,6 +14,6 @@ public class HelloWorldController {
 	
 	@RequestMapping(method=RequestMethod.GET,path="hello-world")
 	public String helloworld() {
-		return "Hello";
+		return "Vikash Verma";
 	}
 }
