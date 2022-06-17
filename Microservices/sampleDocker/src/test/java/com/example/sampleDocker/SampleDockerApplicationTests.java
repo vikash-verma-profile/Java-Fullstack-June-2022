5ed7b0109240757517c2467c884cbebb7ca99756
Level-Up-4 <vikash-verma-profile>
2022-06-17 16:59:00 +0530

    added repo

---
+++ Microservices/sampleDocker/src/test/java/com/example/sampleDocker/SampleDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
