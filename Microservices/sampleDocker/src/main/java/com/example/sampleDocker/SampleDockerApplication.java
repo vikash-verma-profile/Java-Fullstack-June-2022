a4852c20057787729e7e2e93f19c822f50a3aca9
Level-Up-4 <vikash-verma-profile>
2022-06-17 18:14:12 +0530

    added docker file

--- Microservices/sampleDocker/src/main/java/com/example/sampleDocker/SampleDockerApplication.java
+++ Microservices/sampleDocker/src/main/java/com/example/sampleDocker/SampleDockerApplication.java
@@ -2,10 +2,18 @@ package com.example.sampleDocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class SampleDockerApplication {
 
+	@GetMapping("message")
+	public String getMessage() {
+		return "Welcome to Docker";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SampleDockerApplication.class, args);
 	}
