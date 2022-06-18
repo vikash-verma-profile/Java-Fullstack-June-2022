e858aeb04e034a251c9d1fb82bed4c4dc0f38297
Level-Up-4 <vikash-verma-profile>
2022-06-18 19:50:07 +0530

    added code

---
+++ HelloWorld/src/main/java/com/sample/helloworld/HelloWorldWithRequestHandler.java
@@ -0,0 +1,12 @@
+package com.sample.helloworld;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class HelloWorldWithRequestHandler implements RequestHandler<Object, Object> {
+
+    @Override
+    public Object handleRequest(Object input, Context context) {
+        return String.format("Hello %s%s.", input, " from " + context.getFunctionName());
+    }
+} 
